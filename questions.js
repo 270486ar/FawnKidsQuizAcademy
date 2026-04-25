bd0079ca37067a35a0ee8af6dd2f1808e525dca8
Quizacdemy <270486ar@gmail.com>
2026-04-25 16:27:08 +0530

    Add files via upload

--- questions.js
+++ questions.js
@@ -1,17 +1,46 @@
-const quiz = [
-  {
-    q: "Capital of India?",
-    options: ["Mumbai", "Delhi", "Chennai", "Kolkata"],
-    answer: "Delhi"
+const allQuestions = {
+
+  english: {
+    eng01: [
+      { q: "Synonym of Happy?", options: ["Sad","Glad","Angry","Cry"], answer: "Glad" },
+      { q: "Opposite of Big?", options: ["Large","Huge","Small","Tall"], answer: "Small" }
+    ],
+
+    eng02: [
+      { q: "Plural of Child?", options: ["Childs","Children","Childes","Child"], answer: "Children" },
+      { q: "Verb in sentence?", options: ["Run","Book","Pen","Chair"], answer: "Run" }
+    ]
   },
-  {
-    q: "5 + 3 = ?",
-    options: ["6", "7", "8", "9"],
-    answer: "8"
+
+  math: {
+    math01: [
+      { q: "5 + 3 = ?", options: ["6","7","8","9"], answer: "8" },
+      { q: "10 - 4 = ?", options: ["5","6","7","8"], answer: "6" }
+    ],
+
+    math02: [
+      { q: "6 x 2 = ?", options: ["10","11","12","13"], answer: "12" }
+    ]
   },
-  {
-    q: "Sun rises in?",
-    options: ["West", "East", "North", "South"],
-    answer: "East"
+
+  science: {
+    sci01: [
+      { q: "Water formula?", options: ["H2O","CO2","O2","NaCl"], answer: "H2O" }
+    ],
+
+    sci02: [
+      { q: "Planet nearest to Sun?", options: ["Earth","Mars","Mercury","Venus"], answer: "Mercury" }
+    ]
+  },
+
+  gk: {
+    gk01: [
+      { q: "Capital of India?", options: ["Mumbai","Delhi","Chennai","Kolkata"], answer: "Delhi" }
+    ],
+
+    gk02: [
+      { q: "National animal of India?", options: ["Lion","Tiger","Elephant","Horse"], answer: "Tiger" }
+    ]
   }
-];
+
+};
